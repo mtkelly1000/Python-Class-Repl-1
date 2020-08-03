06e28abec0eb79053512e3f9c2d54f9b189b4973
Madison K <mtkelly1000@gmail.com>
2020-08-03 20:00:30 +0000

    added dictionary and if/else stuff

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 for i in range (10):
   print ('jello')
+  print (i)
+print ('bye')
 
+# i is a variable, something that can change
 
+# conditionals: if/else statements
 
+x = 15
+if x == 15:
+  # use two = 
+    print (x)
+else:
+  print ('it\'s not x')
 
+# use backslash if you need to use ' in your sentence without ending the code
+# no caps when you 
+# elif= else + if
 
+# dictionary
 
+book = {
+  'hello':'a greeting'}
+
+print('hello: ' + book.get('hello'))
+
+# has to be book.get
+# to combine two strings together, use a +
