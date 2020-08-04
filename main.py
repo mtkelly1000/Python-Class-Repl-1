07f90f255d41a3a5e467f841745e51ed8cdfc2f1
Madison K <mtkelly1000@gmail.com>
2020-08-04 20:26:53 +0000

    added comments

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 for i in range (2):
   print ('jello')
-print ('bye')
+  print (i)
 
 # i is a variable, something that can change
 # can use "print (i)" to print the number of times that the variable was printed
+# you can say "print (i)" to see the number of times the word prints (starting from 0). if you dont want ti to start from 0, say "print (i+1)"
+
+
+
 
 # conditionals: if/else statements
 
@@ -15,8 +19,11 @@ else:
   print ('it\'s not x')
 
 # use backslash if you need to use ' in your sentence without ending the code
-# no caps when you 
+# no caps when you write code, unless variables
 # elif= else + if
+
+
+
 
 # dictionary
 
@@ -27,6 +34,11 @@ print('hello: ' + book.get('hello'))
 
 # has to be book.get
 # to combine two strings together, use a +
+# if you need to use multiple strings, use a comma outside of the apostrophes
+
+
+
+
 
 
 
